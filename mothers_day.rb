f9fe16d26110ec08bd1f02c542322074660dce81
NOORABDELHALIM <nur.abdelhalim@rebels.ridgenet.org>
2018-08-07 16:23:53 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,7 +1,7 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
 def mothers_day(your_name)
-  puts "Happy Mother's Day, #{our_name}!"
+  puts "Happy Mother's Day, #{your_name}!"
 end
 
 mothers_day("Lacey")
